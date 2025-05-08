fc18e918a2c6adfe75409b9cc8efbc3af79ffcd6
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-05-08 21:42:01 +0330

    Import necessary libraries for data

--- main.py
+++ main.py
@@ -2,3 +2,7 @@
 Main Execution Script
 Runs the complete pipeline from data to evaluation
 """
+from data_preprocessing import load_data
+from models.base_model import build_base_model
+from train import train_model
+from evaluate import evaluate_model
